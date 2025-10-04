c3b030d9f78e22c0ebf1d1dc5f785d7b16dcc5ca
owenhochwald <owenhochwald@gmail.com>
2025-10-04 14:36:18 -0700

    add RunningInstances table config

--- cdk/lib/constructs/dynamodb-tables.ts
+++ cdk/lib/constructs/dynamodb-tables.ts
@@ -1,21 +1,37 @@
 import { Construct } from "constructs";
-import { Table, AttributeType, BillingMode } from "aws-cdk-lib/aws-dynamodb";
+import { Table, AttributeType, BillingMode, ProjectionType } from "aws-cdk-lib/aws-dynamodb";
 import { RemovalPolicy } from "aws-cdk-lib";
 
 export class DynamoDbTables extends Construct {
-  // Add your DynamoDB tables here as you need them
-  // Example:
-  // public readonly usersTable: Table;
-  // public readonly ordersTable: Table;
+    public runningInstancesTable: Table;
 
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
 
-    // Example table - uncomment and modify as needed
-    // this.usersTable = new Table(this, "UsersTable", {
-    //   partitionKey: { name: "userId", type: AttributeType.STRING },
-    //   billingMode: BillingMode.PAY_PER_REQUEST,
-    //   removalPolicy: RemovalPolicy.DESTROY, // Use RETAIN for production
-    // });
-  }
+        this.setupRunningInstances()
+    }
+
+    /**
+    * Schema: instanceId (PK), instanceArn, ebsVolumes (list), creationTime, 
+    *         status, region, instanceType, lastModifiedTime
+    */
+    setupRunningInstances(): void {
+        this.runningInstancesTable = new Table(this, "RunningInstances", {
+            partitionKey: { name: "instanceId", type: AttributeType.STRING },
+            pointInTimeRecovery: true,
+            billingMode: BillingMode.PAY_PER_REQUEST,
+            // TODO: add environment based removal policy config
+            removalPolicy: RemovalPolicy.DESTROY, // Use RETAIN for production
+        });
+
+        // TODO future: add autoscaling group
+        // TODO: or add grantX to specific lambda functions
+
+        this.runningInstancesTable.addGlobalSecondaryIndex({
+            indexName: "StatusCreationTimeIndex",
+            partitionKey: { name: "status", type: AttributeType.STRING },
+            sortKey: { name: "creationTime", type: AttributeType.STRING },
+            projectionType: ProjectionType.ALL,
+        });
+    }
 }
