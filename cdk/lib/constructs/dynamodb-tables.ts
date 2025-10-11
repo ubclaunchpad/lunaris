1f8d2eae346b246bf082605f792e285b0fdb8295
Raiyan Khan <raiyankhan2002@gmail.com>
2025-10-11 14:43:30 -0400

    added runningstreams setup in cdk

--- cdk/lib/constructs/dynamodb-tables.ts
+++ cdk/lib/constructs/dynamodb-tables.ts
@@ -8,8 +8,23 @@ export class DynamoDbTables extends Construct {
   // public readonly usersTable: Table;
   // public readonly ordersTable: Table;
 
+  public RunningStreams: Table;
+
   constructor(scope: Construct, id: string) {
     super(scope, id);
+
+    /*
+     * RunningStreams Table Schema:
+      * - instanceArn (string) - Partition Key
+      * - userId (string)
+      * - streamingId (string) 
+      * - streamingLink (string)
+     */
+    this.RunningStreams = new Table(this, "RunningStreams", {
+      partitionKey: { name: "instanceArn", type: AttributeType.STRING },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY, // Use RETAIN for production
+    });
 
     // Example table - uncomment and modify as needed
     // this.usersTable = new Table(this, "UsersTable", {
