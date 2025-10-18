8f423410edea00fbb044ac60476d9b2f0e082299
Raiyan Khan <raiyankhan2002@gmail.com>
2025-10-18 11:49:39 -0700

    make constructor style similar to previous PR

--- cdk/lib/constructs/dynamodb-tables.ts
+++ cdk/lib/constructs/dynamodb-tables.ts
@@ -3,34 +3,27 @@ import { Table, AttributeType, BillingMode } from "aws-cdk-lib/aws-dynamodb";
 import { RemovalPolicy } from "aws-cdk-lib";
 
 export class DynamoDbTables extends Construct {
-  // Add your DynamoDB tables here as you need them
-  // Example:
-  // public readonly usersTable: Table;
-  // public readonly ordersTable: Table;
-
-  public RunningStreams: Table;
+  public RunningStreamsTable: Table;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
-
-    /*
-     * RunningStreams Table Schema:
-      * - instanceArn (string) - Partition Key
-      * - userId (string)
-      * - streamingId (string) 
-      * - streamingLink (string)
-     */
-    this.RunningStreams = new Table(this, "RunningStreams", {
-      partitionKey: { name: "instanceArn", type: AttributeType.STRING },
-      billingMode: BillingMode.PAY_PER_REQUEST,
-      removalPolicy: RemovalPolicy.DESTROY, // Use RETAIN for production
-    });
-
-    // Example table - uncomment and modify as needed
-    // this.usersTable = new Table(this, "UsersTable", {
-    //   partitionKey: { name: "userId", type: AttributeType.STRING },
-    //   billingMode: BillingMode.PAY_PER_REQUEST,
-    //   removalPolicy: RemovalPolicy.DESTROY, // Use RETAIN for production
-    // });
+    this.setUpRunningStreamsTable();
   }
+
+      /*
+        * RunningStreams Table Schema:
+        * - instanceArn (string) - Partition Key
+        * - userId (string)
+        * - streamingId (string) 
+        * - streamingLink (string)
+        * - createdAt (ISO 8601 formatted date string)
+        * - updatedAt (ISO 8601 formatted date string)
+      */
+      setUpRunningStreamsTable(): void {
+        this.RunningStreamsTable = new Table(this, "RunningStreams", {
+          partitionKey: { name: "instanceArn", type: AttributeType.STRING },
+          billingMode: BillingMode.PAY_PER_REQUEST,
+          removalPolicy: RemovalPolicy.DESTROY, // Use RETAIN for production
+      });
+    }
 }
