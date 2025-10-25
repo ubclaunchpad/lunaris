7a568a624d252abfa1194a4df2cc81c4f26e7d8b
Liu, Jonathan <jonathon.liu024@gmail.com>
2025-10-25 11:55:51 -0700

    fixed merge conflict

--- cdk/lib/constructs/dynamodb-table-props.ts
+++ cdk/lib/constructs/dynamodb-table-props.ts
@@ -1,20 +1,11 @@
 import { AttributeType } from "aws-cdk-lib/aws-dynamodb";
 import { Table } from "aws-cdk-lib/aws-dynamodb";
-import {
-  indexKey,
-  indexSortKey,
-  indexName,
-  indexType,
-} from "aws-cdk-lib/aws-dynamodb";
+("aws-cdk-lib/aws-dynamodb");
 
 export interface DynamoDbWrapperProps {
   tableName: string;
   partitionKey: string;
   sortKey?: string;
-  indexKey?: string;
-  indexSortKey?: string;
-  indexName?: string;
-  indexType?: string;
   indexKey?: string;
   indexSortKey?: string;
   indexName?: string;
